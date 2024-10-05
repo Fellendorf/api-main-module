2d42e3e262c7f09d8d12a8587c592eae0f4cabe6
Fellendorf <finn85fellendorf@gmail.com>
2024-10-06 02:34:49 +0300

    Mocked "topics" controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TopicsController } from './controllers/topics/topics.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, TopicsController],
   providers: [AppService],
 })
 export class AppModule {}
