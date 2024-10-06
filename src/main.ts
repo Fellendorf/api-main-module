d1b59c964353e4d0af53c0a355ec33aac4cd7a1d
Fellendorf <finn85fellendorf@gmail.com>
2024-10-07 00:19:59 +0300

    ENV_VARS: bug fix

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import { envUtils } from './utils/env.utils';
 
-const CORS_ENABLED = Boolean(process.env.CORS_ENABLED) || false;
-const CORS_ORIGIN = process.env.CORS_ORIGIN;
+const CORS_ENABLED =
+  envUtils.convertToBoolean(process.env.CORS_ENABLED) || false;
+const CORS_ORIGIN = envUtils.convertToArray(process.env.CORS_ORIGIN) || '*';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   if (CORS_ENABLED) {
     app.enableCors({ origin: CORS_ORIGIN });
   }
