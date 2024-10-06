de6495b46c5dc26fb44ddb3db22cb31e8ab41d27
Fellendorf <finn85fellendorf@gmail.com>
2024-10-06 13:22:37 +0300

    CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+
+const CORS_ENABLED = Boolean(process.env.CORS_ENABLED) || false;
+const CORS_ORIGIN = process.env.CORS_ORIGIN;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  if (CORS_ENABLED) {
+    app.enableCors({ origin: CORS_ORIGIN });
+  }
   await app.listen(3000);
 }
 bootstrap();
